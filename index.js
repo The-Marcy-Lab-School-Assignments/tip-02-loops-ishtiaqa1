aabcfbe26715ecd086ea4606602d8027c56d5eb1
ishtiaqa1 <ishtiaqa2003@gmail.com>
2025-10-01 10:42:14 -0400

    Added an accumulated total in index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
   //write code here
+  let acc = 0;
   for (let i = 1; i <= n; i++) {
+    acc += Math.floor(Math.pow(i, 2));
     console.log(Math.floor(Math.pow(i, 2)));
   }
+  console.log(`Final output is: ${acc}`);
+  return acc;
 }
 printSquareNumbers(5);
 //will print: 1, 4, 9, 16, 25
