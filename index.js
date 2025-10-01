e8b8c866f0a91e569ccc6d93c0f1c175324c1fde
ishtiaqa1 <ishtiaqa2003@gmail.com>
2025-10-01 10:37:03 -0400

    Finished the problem set

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
 const printSquareNumbers = (n) => {
   //write code here
-
+  for (let i = 1; i <= n; i++) {
+    console.log(Math.floor(Math.pow(i, 2)));
+  }
 }
 printSquareNumbers(5);
 //will print: 1, 4, 9, 16, 25
